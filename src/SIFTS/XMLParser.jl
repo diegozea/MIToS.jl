c9ad4639cdd69fbfa59ee20d2585c3b84ef190fc
Diego Javier Zea <diegozea@gmail.com>
2015-08-25 14:42:10 -0300

    Cleaning SIFTS

--- src/SIFTS/XMLParser.jl
+++ src/SIFTS/XMLParser.jl
@@ -16,7 +16,8 @@ end
 # =========================
 
 """
-Gets the entities of a SIFTS XML. Each entity is a PDB chain. For example:
+Gets the entities of a SIFTS XML. In some cases, each entity is a PDB chain.
+WARNING: Sometimes there are more chains than entities!
 ```
 <entry dbSource="PDBe" ...
   ...
@@ -35,52 +36,11 @@ function _get_entities(filename)
 	get_elements_by_tagname(siftsroot, "entity")
 end
 
-# """
-# Returns the protein entity with PDB `chain` as `entityId` from the array of entities.
-# WARNING: Sometimes there are more chains than entities!
-# """
-# function _get_entity(entities, chain::ASCIIString)
-#   for entity in entities
-#     # XML: <entity type="protein" entityId="A">
-#     if attribute(entity, "entityId") == chain && attribute(entity, "type") == "protein"
-#       return(entity)
-#     end
-#   end
-# end
-
 """
 Gets an array of the segments, the continuous region of an entity.
 Chimeras and expression tags generates more than one segment for example.
 """
 _get_segments(entity) = get_elements_by_tagname(entity, "segment")
-
-"""
-Returns the difference between start and end, for example `6` for:
-```
-<segment segId="2vqc_A_1_7" start="1" end="7">
-```
-"""
-_length_segment(segment) = parse(Int, attribute(segment, "end")) - parse(Int, attribute(segment, "start"))
-
-"""
-Returns an `Int` with the index of the largest segment.
-For example returns 2 for 2VQC:
-```
-1 : 2vqc_A_1_7
-2 : 2vqc_A_8_118
-```
-"""
-function _choose_largest(segments)
-  j = 0
-  len_before = 0
-  for i in 1:length(segments)
-    len_now = _length_segment(segments[i])
-    if len_now > len_before
-      j = i
-    end
-  end
-  j
-end
 
 """
 Returns an Iterator of the residues on the listResidue
