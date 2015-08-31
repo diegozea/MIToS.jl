660c71dad79bda43822840bb03aded00e0d348ce
Javier Iserte <jiserte@unq.edu.ar>
2015-08-31 18:44:15 -0300

    Revert "Revert "Added some documentation to functions.""
    This reverts commit daa8e0e7bfb0ff2b6ed71fb19dd1327c3c7a8d8d.

--- src/Clustering/Clustering.jl
+++ src/Clustering/Clustering.jl
@@ -6,6 +6,15 @@ module Clustering
 
   percentidentity, percentidentity2,
 
+  """
+  Cluster sequences using Hobohm method 1.
+ 
+  For more information read: 
+
+  1. Protein Sci. 1992 Mar;1(3):409-17.
+  Selection of representative protein data sets.
+  Hobohm U(1), Scharf M, Schneider R, Sander C.
+  """
   hobohmI
 
   include("Clusters.jl")
