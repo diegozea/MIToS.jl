11ea31e8f8402e1b6a285cf80a63289f055a0d88
Diego Javier Zea <diegozea@gmail.com>
2016-03-16 18:05:11 -0300

    SIFTS doc

--- src/SIFTS/SIFTS.jl
+++ src/SIFTS/SIFTS.jl
@@ -1,3 +1,22 @@
+"""
+The `SIFTS` module of MIToS allows to obtain the
+residue-level mapping between databases stored in the SIFTS XML files.
+It makes easy to assign PDB residues to UniProt/Pfam positions.
+Given the fact that pairwise alignments can lead to misleading association between residues in both sequences,
+SIFTS offers  more reliable association between sequence and structure residue numbers.
+
+
+**Features**
+
+- Download and parse SIFTS XML files
+- Store residue-level mapping in Julia
+- Easy generation of `Dict`s between residues numbers
+
+```julia
+
+using MIToS.SIFTS
+```
+"""
 module SIFTS
 
   using LightXML
