ca53bddb9010f58db4e183577d23a667eb12f852
Diego Javier Zea <diegozea@gmail.com>
2016-03-29 16:33:58 -0300

    WIP: Docs

--- mitos.js
+++ mitos.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
   // http://stackoverflow.com/questions/17341122/link-and-execute-external-javascript-file-hosted-on-github
   var raw = "https://cdn.rawgit.com/diegozea/MIToS.jl/master/docs/";
 
+  var navheader_source   = $("#header-template").html();
+  var navheader_template = Handlebars.compile(navheader_source);
+
   var navcol_source   = $("#navcol-template").html();
   var navcol_template = Handlebars.compile(navcol_source);
 
@@ -12,6 +15,9 @@ $(document).ready(function(){
   var navcol_html     = navcol_template(pages);
   $("#NavCol").html(navcol_html)
 
+  var navheader_html     = navheader_template(pages);
+  $("#NavHeader").html(navheader_html)
+
   fillcontent = function(direction){
         $("#includedContent").load(direction);
   };
@@ -19,19 +25,14 @@ $(document).ready(function(){
   fillcontent("home.html");
 
   $(".link").click(function(){
-
+    var direction = $(this).data("src");
+    fillcontent(direction);
     $(".link").removeClass("active");
     $(this).addClass("active");
-
-    var direction = $(this).data("src");
-
-    fillcontent(direction);
   });
 
-  $(".internallink").click(function(){
-
+  $(".menulink").click(function(){
     var direction = $(this).data("src");
-
     fillcontent(direction);
   });
 
