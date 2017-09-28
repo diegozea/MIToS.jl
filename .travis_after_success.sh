a8b5f9e32d3e1cebb8256e9dd35e28e9757a8c00
diegozea <dzea@slim@gitlab.aire>
2017-09-28 17:07:45 +0200

    try to solve test related bugs

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -3,9 +3,10 @@ then
     # COVERAGE
     julia -e 'cd(Pkg.dir("MIToS")); Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder()); Codecov.submit(Codecov.process_folder())'
     # DOCS
-    julia -e 'Pkg.add("Documenter")'
-    julia -e 'Pkg.add("Plots")' # Plots
+    julia -e 'ENV["PYTHON"] = ""; Pkg.add("PyCall"); Pkg.build("PyCall")' # PyCall
     julia -e 'ENV["PYTHON"] = ""; Pkg.add("PyPlot"); Pkg.build("PyPlot")' # PyPlot
+    julia -e 'Pkg.add("Plots")' # Plots
     julia -e 'Pkg.add("DataFrames"); Pkg.add("StatPlots")' # These are used in MSA.md
+    julia -e 'Pkg.add("Documenter")'
     julia -e 'cd(Pkg.dir("MIToS")); include(joinpath("docs", "make.jl"))'
 fi
