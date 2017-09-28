3b152d10bdba56ace398d90c1d758874c65b4d3f
Diego Javier Zea <diegozea@gmail.com>
2017-09-28 09:12:36 +0200

    after success script for travis

---
+++ .travis_after_success.sh
@@ -0,0 +1,13 @@
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]
+    # COVERAGE
+    - julia -e 'cd(Pkg.dir("MIToS")); Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder()); Codecov.submit(Codecov.process_folder())'
+    # DOCS
+    if [[ "$TRAVIS_BRANCH" != "master" ]]
+    then
+        julia -e 'Pkg.add("Documenter")'
+        julia -e 'Pkg.add("Plots")' # Plots
+        julia -e 'ENV["PYTHON"] = ""; Pkg.add("PyPlot"); Pkg.build("PyPlot")' # PyPlot
+        julia -e 'Pkg.add("DataFrames"); Pkg.add("StatPlots")' # These are used in MSA.md
+        julia -e 'cd(Pkg.dir("MIToS")); include(joinpath("docs", "make.jl"))'
+    fi
+fi
