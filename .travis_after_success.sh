04f32caf94c37edfa479944c5c3612a2f51b4ac8
diegozea <dzea@slim@gitlab.aire>
2017-09-28 16:39:26 +0200

    Update after success script

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -3,12 +3,9 @@ then
     # COVERAGE
     julia -e 'cd(Pkg.dir("MIToS")); Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder()); Codecov.submit(Codecov.process_folder())'
     # DOCS
-    if [[ "$TRAVIS_BRANCH" != "master" ]]
-    then
-        julia -e 'Pkg.add("Documenter")'
-        julia -e 'Pkg.add("Plots")' # Plots
-        julia -e 'ENV["PYTHON"] = ""; Pkg.add("PyPlot"); Pkg.build("PyPlot")' # PyPlot
-        julia -e 'Pkg.add("DataFrames"); Pkg.add("StatPlots")' # These are used in MSA.md
-        julia -e 'cd(Pkg.dir("MIToS")); include(joinpath("docs", "make.jl"))'
-    fi
+    julia -e 'Pkg.add("Documenter")'
+    julia -e 'Pkg.add("Plots")' # Plots
+    julia -e 'ENV["PYTHON"] = ""; Pkg.add("PyPlot"); Pkg.build("PyPlot")' # PyPlot
+    julia -e 'Pkg.add("DataFrames"); Pkg.add("StatPlots")' # These are used in MSA.md
+    julia -e 'cd(Pkg.dir("MIToS")); include(joinpath("docs", "make.jl"))'
 fi
