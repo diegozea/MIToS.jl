8d6b175c33370630969a5412948ece4b52111129
Diego Javier Zea <diegozea@gmail.com>
2018-07-28 02:12:59 +0200

    Solve HDF5 and PyPlot problems in Travis-CI

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -3,9 +3,10 @@ then
     # COVERAGE
     julia -e 'cd(Pkg.dir("MIToS")); Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder()); Codecov.submit(Codecov.process_folder())'
     # DOCS
-    sudo python -m pip install --upgrade pip # PyPlot
-    sudo pip install --upgrade matplotlib # PyPlot
-    julia -e 'Pkg.add("PyPlot"); Pkg.build("PyPlot")' # PyPlot
+    # sudo python -m pip install --upgrade pip # PyPlot
+    # sudo pip install --upgrade matplotlib # PyPlot
+    julia -e 'Pkg.add("PyCall"); ENV["PYTHON"]=""; Pkg.build("PyCall")'
+    julia -e 'Pkg.add("PyPlot"); ENV["PYTHON"]=""; Pkg.build("PyPlot")' # PyPlot
     julia -e 'Pkg.add("Plots")' # Plots
     julia -e 'Pkg.add("PlotRecipes")' # PlotRecipes
     julia -e 'Pkg.checkout("PlotRecipes")' # PlotRecipes
