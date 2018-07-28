18a24f820262d6371b06a55a90c3bef4d2d89314
Diego Javier Zea <diegozea@gmail.com>
2018-07-28 02:51:37 +0200

    Solve PyPlot error in doc generation

--- .travis_after_success.sh
+++ .travis_after_success.sh
@@ -5,13 +5,17 @@ then
     # DOCS
     # sudo python -m pip install --upgrade pip # PyPlot
     # sudo pip install --upgrade matplotlib # PyPlot
-    julia -e 'Pkg.add("PyCall"); ENV["PYTHON"]=""; Pkg.build("PyCall")'
-    julia -e 'Pkg.add("PyPlot"); ENV["PYTHON"]=""; Pkg.build("PyPlot")' # PyPlot
+    # Work around matplotlib backend error leading to "matplotlib.pyplot could not be found by pyimport": can't just set env PYTHON="".
+    julia -e 'ENV["PYTHON"]=""; Pkg.add("Conda"); using Conda; Conda.update(); Conda.add("matplotlib"); Pkg.add("PyCall"); Pkg.build("PyCall"); Pkg.add("PyPlot");'
+    export LD_LIBRARY_PATH=$HOME/.julia/v0.6/Conda/deps/usr/lib
+    julia -e 'using PyPlot' # PyPlot
     julia -e 'Pkg.add("Plots")' # Plots
     julia -e 'Pkg.add("PlotRecipes")' # PlotRecipes
     julia -e 'Pkg.checkout("PlotRecipes")' # PlotRecipes
     julia -e 'Pkg.add("DataFrames"); Pkg.add("StatPlots")' # These are used in MSA.md
     julia -e 'Pkg.add("Documenter")'
-    echo "DOCS"
+    echo "=============="
+    echo "  DOCUMENTER  "
+    echo "=============="
     julia -e 'cd(Pkg.dir("MIToS")); include(joinpath("docs", "make.jl"))'
 fi
