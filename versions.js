984679eba50c57442fb35c295459affcd0bd8be1
autodocs <autodocs>
2018-12-04 00:40:48 +0000

    build based on d4de04b

--- versions.js
+++ versions.js
@@ -1,13 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
-  "latest",
-  "release-2.2",
-  "release-2.1",
-  "release-2.0",
   "v2.2",
-  "v2.2.0",
-  "v2.1.2",
-  "v2.1.1",
-  "v2.1.0",
-  "v2.0.0",
+  "v2.1",
+  "v2.0",
+  "dev",
 ];
