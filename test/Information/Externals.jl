f0af11d720dcba06e6b44b9093c85671c52f58f4
Diego Javier Zea <diegozea@gmail.com>
2020-02-22 22:53:02 +0100

    Use PackageSpec for Julia 1.5.0-DEV

--- test/Information/Externals.jl
+++ test/Information/Externals.jl
@@ -1,6 +1,6 @@
 using Pkg
 
-Pkg.develop("https://github.com/carlobaldassi/GaussDCA.jl")
+Pkg.develop(PackageSpec(url=https://github.com/carlobaldassi/GaussDCA.jl"))
 
 msa = map(Residue, rand(1:21,100,20))
 dca = gaussdca(msa, min_separation=2)
