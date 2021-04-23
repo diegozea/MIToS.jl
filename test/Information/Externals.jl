96f16c94f11df161a2169a31d5c8832145ffec2a
Diego Javier Zea <diegozea@gmail.com>
2021-04-23 12:46:07 +0200

    GaussDCA needs Julia >= 1.5.0

--- test/Information/Externals.jl
+++ test/Information/Externals.jl
@@ -1,10 +1,12 @@
-using Pkg
+if VERSION >= v"1.5.0"
+    using Pkg
 
-Pkg.develop(PackageSpec(url="https://github.com/carlobaldassi/GaussDCA.jl"))
+    Pkg.develop(PackageSpec(url="https://github.com/carlobaldassi/GaussDCA.jl"))
 
-msa = map(Residue, rand(1:21,100,20))
-dca = gaussdca(msa, min_separation=2)
+    msa = map(Residue, rand(1:21,100,20))
+    dca = gaussdca(msa, min_separation=2)
 
-@test  isnan(dca[1,1])
-@test  isnan(dca[1,2])
-@test !isnan(dca[1,3])
+    @test  isnan(dca[1,1])
+    @test  isnan(dca[1,2])
+    @test !isnan(dca[1,3])
+end
