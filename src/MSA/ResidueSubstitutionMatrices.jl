a50a63f76bb952231bab870dd7c3082a5f0c7c01
Diego Javier Zea <diegozea@gmail.com>
2025-06-12 16:36:19 +0200

    Refactor ResidueSubstitutionMatrix to accept AbstractMatrix and improve display functionality

--- src/MSA/ResidueSubstitutionMatrices.jl
+++ src/MSA/ResidueSubstitutionMatrices.jl
@@ -1,11 +1,12 @@
 module ResidueSubstitutionMatrices
 
 using ..MSA: Residue, ResidueAlphabet, GappedAlphabet
+using NamedArrays
 
 export ResidueSubstitutionMatrix
 
 """
-    ResidueSubstitutionMatrix{T<:Real}(scores::Matrix{T}, alphabet::ResidueAlphabet = GappedAlphabet())
+    ResidueSubstitutionMatrix{T<:Real}(scores::AbstractMatrix{T}, alphabet::ResidueAlphabet = GappedAlphabet())
 
 Symmetric substitution scores for pairs of [`Residue`](@ref) values.
 The matrix must be square and its size must match the length of the
@@ -17,10 +18,10 @@ struct ResidueSubstitutionMatrix{T<:Real,A<:ResidueAlphabet} <: AbstractMatrix{T
     alphabet::A
 
     function (::Type{ResidueSubstitutionMatrix{T,A}})(
-    alphabet::A = GappedAlphabet(),
-Base.show(io::IO, matrix::ResidueSubstitutionMatrix) =
-    show(io, MIME"text/plain"(), matrix)
-
+        scores::AbstractMatrix{T},
+        alphabet::A,
+    ) where {T<:Real,A<:ResidueAlphabet}
+        n, m = size(scores)
         if n != m
             throw(ArgumentError("scores matrix must be square"))
         end
@@ -36,13 +37,18 @@ Base.size(matrix::ResidueSubstitutionMatrix) = size(matrix.scores)
 @inline Base.getindex(matrix::ResidueSubstitutionMatrix, I::Vararg{Int,N}) where {N} =
     matrix.scores[I...]
 
-ResidueSubstitutionMatrix(scores::Matrix{T}) where {T<:Real} =
-    ResidueSubstitutionMatrix(scores, GappedAlphabet())
-
 ResidueSubstitutionMatrix(
-    scores::Matrix{T},
-    alphabet::A,
+    scores::AbstractMatrix{T},
+    alphabet::A = GappedAlphabet(),
 ) where {T<:Real,A<:ResidueAlphabet} = ResidueSubstitutionMatrix{T,A}(scores, alphabet)
+
+"""
+    matrix[i::Int, j::Int]
+
+Return the substitution score for the pair of residue indexes `i` and `j`.
+"""
+@inline Base.getindex(matrix::ResidueSubstitutionMatrix{T}, i::Int, j::Int) where {T} =
+    matrix.scores[i, j]
 
 """
     matrix[a::Residue, b::Residue]
@@ -50,6 +56,15 @@ ResidueSubstitutionMatrix(
 Return the substitution score for the pair of residues `a` and `b`.
 """
 @inline function Base.getindex(
+    matrix::ResidueSubstitutionMatrix{T},
+    a::Residue,
+    b::Residue,
+) where {T}
+    i = matrix.alphabet[a]
+    j = matrix.alphabet[b]
+    @inbounds matrix.scores[i, j]
+end
+
 @inline function Base.getindex(
     matrix::ResidueSubstitutionMatrix{T},
     r::Residue,
@@ -68,18 +83,13 @@ end
     @inbounds matrix.scores[r, j]
 end
 
+function Base.show(io::IO, ::MIME"text/plain", matrix::ResidueSubstitutionMatrix)
     labels = names(matrix.alphabet)
     named = NamedArray(matrix.scores, (labels, labels))
     show(io, MIME"text/plain"(), named)
-    i = matrix.alphabet[a]
-    j = matrix.alphabet[b]
-    @inbounds matrix.scores[i, j]
 end
 
-function Base.show(io::IO, ::MIME"text/plain", matrix::ResidueSubstitutionMatrix)
-    println(io, typeof(matrix), " : ")
-    println(io, "Alphabet: ", matrix.alphabet)
-    show(io, matrix.scores)
-end
+Base.show(io::IO, matrix::ResidueSubstitutionMatrix) =
+    show(io, MIME"text/plain"(), matrix)
 
 end # module
