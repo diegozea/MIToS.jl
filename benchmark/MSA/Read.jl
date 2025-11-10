104144646702c940750406bc11aee1f150c53f21
Diego Javier Zea <diegozea@gmail.com>
2025-11-10 23:45:34 +0100

    Add benchmarkable tests for annotations in MSA Read.jl

--- benchmark/MSA/Read.jl
+++ benchmark/MSA/Read.jl
@@ -5,6 +5,21 @@ let
 
     SUITE["MSA"]["read"]["Stockholm"] =
         @benchmarkable read_file($sth, Stockholm, MultipleSequenceAlignment)
+    SUITE["MSA"]["read"]["Stockholm_annotated"] =
+        @benchmarkable read_file($sth, Stockholm, AnnotatedMultipleSequenceAlignment)
+    SUITE["MSA"]["read"]["Stockholm_mapping"] = @benchmarkable read_file(
+        $sth,
+        Stockholm,
+        AnnotatedMultipleSequenceAlignment,
+        generatemapping = true,
+    )
+    SUITE["MSA"]["read"]["Stockholm_mapping_coords"] = @benchmarkable read_file(
+        $sth,
+        Stockholm,
+        AnnotatedMultipleSequenceAlignment,
+        generatemapping = true,
+        useidcoordinates = true,
+    )
     SUITE["MSA"]["read"]["FASTA.gz"] =
         @benchmarkable read_file($fasta_gz, FASTA, MultipleSequenceAlignment)
 end
