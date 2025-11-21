5652c4fa13a74059ccf5e2b154a1141d8c94ec7f
Diego Javier Zea <diegozea@gmail.com>
2025-11-21 07:56:41 +0100

    Use ifelse for CB selection and drop BenchmarkTools dependency

--- benchmark/PDB/Distance.jl
+++ benchmark/PDB/Distance.jl
@@ -3,4 +3,8 @@ let
     struc = read_file(pdb_file, PDBFile, model = "1", chain = "A", group = "ATOM")
 
     SUITE["PDB"]["distance"]["1CBN_20_30"] = @benchmarkable distance($struc[20], $struc[30])
+    SUITE["PDB"]["squared_distance"]["1CBN_20_30_heavy"] =
+        @benchmarkable squared_distance($struc[20], $struc[30]; criteria = "Heavy")
+    SUITE["PDB"]["contact"]["1CBN_20_30_heavy"] =
+        @benchmarkable contact($struc[20], $struc[30], 6.05; criteria = "Heavy")
 end
