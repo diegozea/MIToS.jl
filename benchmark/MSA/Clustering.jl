1ce53358d3b1ec4c93167d6638db146d6b964038
Diego Javier Zea <diegozea@gmail.com>
2025-11-28 10:15:56 +0100

    Add benchmarkable tests for hobohmI with different thresholds

--- benchmark/MSA/Clustering.jl
+++ benchmark/MSA/Clustering.jl
@@ -2,5 +2,8 @@ let
     msa_file = joinpath(@__DIR__, "..", "..", "test", "data", "Gaoetal2011.fasta")
     msa = read_file(msa_file, FASTA, MultipleSequenceAlignment)
 
+    SUITE["MSA"]["hobohmI"]["pid20"] = @benchmarkable hobohmI($msa, 20)
     SUITE["MSA"]["hobohmI"]["pid62"] = @benchmarkable hobohmI($msa, 62)
+    SUITE["MSA"]["hobohmI"]["pid80"] = @benchmarkable hobohmI($msa, 80)
+    SUITE["MSA"]["hobohmI"]["pid99"] = @benchmarkable hobohmI($msa, 99)
 end
